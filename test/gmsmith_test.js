8bde6f241a59eaaf9d6dc9bd38f6ea477c4601c6
Todd Wolfson <todd@twolfson.com>
2014-10-02 15:49:23 +0900

    Moved to flag but it isn't implemented yet

--- test/gmsmith_test.js
+++ test/gmsmith_test.js
@@ -1,13 +1,13 @@
 // Load our dependencies
 var spritesmithEngineTest = require('spritesmith-engine-test');
 
-// If we are on Windows, skip over performance test (it cannot handle the long argument string)
-if (process.platform === 'win32') {
-  delete outline['interpretting a ridiculous amount of images'];
-}
-
 // Run our tests
 spritesmithEngineTest.run({
   engine: gmsmith,
-  engineName: 'gmsmith'
+  engineName: 'gmsmith',
+  options: {
+    // If we are on Windows, skip over performance test (it cannot handle the long argument string)
+    // TODO: Implement this flag
+    skipRidiculousImagesTest: process.platform === 'win32'
+  }
 });
