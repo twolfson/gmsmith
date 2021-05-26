6948cb8c26a432fa8046f473de7b6eed24905ab5
Dirk Stolle <striezel-dev@web.de>
2021-05-26 04:29:55 +0200

    update graphicsmagick version in install script to 1.3.36
    The previous version 1.3.17 is not available for download anymore
    on SourceForge.

--- test/install.sh
+++ test/install.sh
@@ -12,9 +12,9 @@ else
   sudo apt-get remove imagemagick -y
 
   # Download, make, and install graphicsmagick
-  wget http://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/1.3.17/GraphicsMagick-1.3.17.tar.xz/download --output-document GraphicsMagick-1.3.17.tar.xz
-  tar xvf GraphicsMagick-1.3.17.tar.xz
-  cd GraphicsMagick-1.3.17
+  wget http://sourceforge.net/projects/graphicsmagick/files/graphicsmagick/1.3.36/GraphicsMagick-1.3.36.tar.xz/download --output-document GraphicsMagick-1.3.36.tar.xz
+  tar xvf GraphicsMagick-1.3.36.tar.xz
+  cd GraphicsMagick-1.3.36
   ./configure
   make
   sudo make install
